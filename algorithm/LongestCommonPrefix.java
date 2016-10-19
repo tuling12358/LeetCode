c90c657203c3bfd9cfbe1f34ddd02cd57d046b19
tuling12358 <tuling12358@users.noreply.github.com>
2016-10-19 23:10:26 +0800

    Update LongestCommonPrefix.java

--- algorithm/LongestCommonPrefix.java
+++ algorithm/LongestCommonPrefix.java
@@ -1,7 +1,7 @@
 
 /**
   Write a function to find the longest common prefix string amongst an array of strings.
-  
+  在字符串数组中找出 最长的 公共前缀
 */
 public class LongestCommonPrefix {
     public String longestCommonPrefix(String[] strs) {
